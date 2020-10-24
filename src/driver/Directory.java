dbbe9fa8c90ce794ccb79a0928c4ab98de5e0628
chancol7 <chancol7@UTORONTO.CA>
2020-10-24 01:48:28 +0000

    added MakeDirectory and ChangeCurrentDirectory + daily meeting file

--- src/driver/Directory.java
+++ src/driver/Directory.java
@@ -2,4 +2,12 @@ package driver;
 
 public class Directory {
 
+  private String [] contents = {};
+
+  public String [] getDirContents() {
+    String [] temp = contents;
+
+    return temp;
+  }
+
 }
