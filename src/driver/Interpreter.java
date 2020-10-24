a999007ace1ef1599a717178b9151319ce685cd0
huynhd12 <huynhd12@UTORONTO.CA>
2020-10-24 02:50:15 +0000

    Updated exit crc card

--- src/driver/Interpreter.java
+++ src/driver/Interpreter.java
@@ -4,10 +4,11 @@ public class Interpreter {
 	
 	public static void interpret(String userCommand, JShell shell) {
 		String message = "";
-		if (userCommand.equals("exit")) {
-			message = Exit.exit(shell);
-		}else{
-			//Filler
+		if (userCommand.equals("exit")) message = Exit.exit(shell);
+		if(true){
+			//I'd imagine that each command class will have a try catch so return(error.getMessage())
+				//in the catch? or each command could also take in shell and print whatever they need?
+				//or each command can just print separately
 		}
 		shell.print(message);
 	}
