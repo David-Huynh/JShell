e57ae39e30d46760beb13e57dfff7c1f84607059
huynhd12 <huynhd12@UTORONTO.CA>
2020-10-24 02:35:10 +0000

    Added exit class functionality to exit JShell when called by Interpreter, added Interpreter functionality to print messages to Shell and to call exit when User inputs 'exit'.

--- src/driver/Interpreter.java
+++ src/driver/Interpreter.java
@@ -3,7 +3,13 @@ package driver;
 public class Interpreter {
 	
 	public static void interpret(String userCommand, JShell shell) {
-		
+		String message = "";
+		if (userCommand.equals("exit")) {
+			message = Exit.exit(shell);
+		}else{
+			//Filler
+		}
+		shell.print(message);
 	}
 	
 }
