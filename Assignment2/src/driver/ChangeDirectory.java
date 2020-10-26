09c80a8906f89f968f7c9543ae680186912e67cb
chancol7@UTORONTO.CA <chancol7@UTORONTO.CA@dfdc42c8-2631-4d2a-ab77-34b069f44838>
2020-10-26 05:00:21 +0000

    implemented MakeDirectory + fixed minor issue with ChangeDirectory performOutcome()

--- Assignment2/src/driver/ChangeDirectory.java
+++ Assignment2/src/driver/ChangeDirectory.java
@@ -6,26 +6,32 @@ public class ChangeDirectory extends ShellCommand {
 
   public static void performOutcome(JShell shell, String[] parameters) {
     Directory currDir = shell.getCurrentDir();
-    String [] directories = parameters[1].split("/");
+    String [] directories = {};
+    if (parameters.length > 1) {
+      directories = parameters[1].split("/");
+    }
 
     for (int i = 0; i < directories.length; i++) {
-      if (directories[i].equals("..")) {
-        currDir = currDir.getParentDir();
-      } else if (directories[i].equals(".")) {
-        currDir = currDir;
+      if (directories[i].equals("..") || directories[i].equals(".")) {
+        if (directories[i].equals("..")) {
+          currDir = currDir.getParentDir();
+        }
       } else {
         ArrayList<StorageUnit> contents = currDir.getDirContents();
+        int valid = 0;
         for (int j = 0; j < contents.size(); j++) {
           if (contents.get(j).name.equals(directories[i])) {
             if (contents.get(j).getClass().getSimpleName().equals("Directory")) {
-              currDir.setParentDir(currDir);
               currDir = (Directory) contents.get(j);
               shell.setCurrentDir(currDir);
+              valid = 1;
+              break;
             }
-          } else {
-            System.out.println("cd: no such directory: "+directories[i]);
-            return;
           }
+        }
+        if (valid == 0) {
+          System.out.println("cd: no such directory: "+directories[i]);
+          return;
         }
       }
     }
--- Assignment2/src/driver/MakeDirectory.java
+++ Assignment2/src/driver/MakeDirectory.java
@@ -1,8 +1,54 @@
 package driver;
 
+import java.util.ArrayList;
+
 public class MakeDirectory extends ShellCommand {
 
   public static void performOutcome(JShell shell, String [] parameters) {
+    Directory currDir = shell.getCurrentDir();
+    String [] dir1 = {};
+    String [] dir2 = {};
+    if (parameters.length == 2) {
+      dir1 = parameters[1].split("/");
+      makeDir(currDir, dir1);
+    } else if (parameters.length == 3) {
+      dir1 = parameters[1].split("/");
+      dir2 = parameters[2].split("/");
+      makeDir(currDir, dir1);
+      makeDir(currDir, dir2);
+    }
+  }
 
+  private static void makeDir(Directory currDir, String [] dir) {
+    for (int i = 0; i < dir.length; i++) {
+      if (i+1 == dir.length) {
+        //create directory
+        Directory newDir = new Directory();
+        newDir.name = dir[i];
+        newDir.setParentDir(currDir);
+      } else {
+        if (dir[i].equals("..") || dir[i].equals(".")) {
+          if (dir[i].equals("..")) {
+            currDir = currDir.getParentDir();
+          }
+        } else {
+          ArrayList<StorageUnit> contents = currDir.getDirContents();
+          int valid = 0;
+          for (StorageUnit content: contents) {
+            if (content.name.equals(dir[i])) {
+              if (content.getClass().getSimpleName().equals("Directory")) {
+                currDir = (Directory) content;
+                valid = 1;
+                break;
+              }
+            }
+          }
+          if (valid == 0) {
+            System.out.println("mkdir: No such directory: "+dir[i-1]+"/"+dir[i]);
+            return;
+          }
+        }
+      }
+    }
   }
 }
