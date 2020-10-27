a03a4187ad712b870f027d2de322647f8e2b9c67
hejeff2 <hejeff2@UTORONTO.CA>
2020-10-27 00:44:00 +0000

    Changed CRC card for PWD, finished code for PrintWorkingDirectory modified some code in JShell to accommodate printing absolute paths

--- src/driver/PrintWorkingDirectory.java
+++ src/driver/PrintWorkingDirectory.java
@@ -1,9 +1,25 @@
 package driver;
 
+import java.util.Stack;
+
 public class PrintWorkingDirectory {
 
   public static void performOutcome(JShell shell) {
     
+    //To print the absolute path, we need to implement a directory stack 
+    
+    //Stack<String> directoryStack = shell.getAbsolutePath();
+    
+    String fullPath = "";
+    
+    Stack<String> directoryStack = null; // remove when directoryStack is actually implemented
+    
+    for(int i = 0; i < directoryStack.size(); i++)
+    {
+      fullPath = directoryStack.get(i) + "\\" + fullPath;
+    }
+    
+    System.out.println(fullPath);
   }
 
 }
