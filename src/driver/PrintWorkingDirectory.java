91a73d3f234e269afbb7c3a0f4711c55fbdc8937
wongnevi <wongnevi@UTORONTO.CA>
2020-10-30 19:36:19 +0000

    Added manuals for popd, pushd, and pwd

--- src/driver/PrintWorkingDirectory.java
+++ src/driver/PrintWorkingDirectory.java
@@ -32,28 +32,34 @@ package driver;
 
 public class PrintWorkingDirectory extends ShellCommand {
 
-  public static void performOutcome(JShell shell) {
-    
-    Directory currDir = shell.getCurrentDir(); //get current directory
-    String fullPath = "";
-    
-    if(currDir != shell.getRootDir())
-    {
-      fullPath = currDir.getParentDir().name + "\\" + currDir.name + fullPath; //append name of parent directory to current directory
-      currDir = currDir.getParentDir();
-    
-        while(currDir != shell.getRootDir())
-        {
-          fullPath = currDir.getParentDir().name + "\\" + fullPath; //keep appending parent directory until we hit root
-          currDir = currDir.getParentDir();
-        }
-        
-        System.out.println(fullPath); //print absolute path
-    }
-    else
-    {
-      System.out.println(shell.getRootDir().name); //print root directory name, since above prints nothing if currDir = root
-    }
-  }
+	public static String getManual() {
+		return "pwd \nPrint the current working directory (including the whole "
+				+ "path).  ";
+	}
+	
+	public static void performOutcome(JShell shell) {
+
+		Directory currDir = shell.getCurrentDir(); // get current directory
+		String fullPath = "";
+
+		if (currDir != shell.getRootDir()) {
+			fullPath = currDir.getParentDir().name + "\\" + currDir.name
+					+ fullPath; // append name of parent directory to current
+								// directory
+			currDir = currDir.getParentDir();
+
+			while (currDir != shell.getRootDir()) {
+				fullPath = currDir.getParentDir().name + "\\" + fullPath;
+				// keep appending parent directory until we hit root
+				currDir = currDir.getParentDir();
+			}
+
+			System.out.println(fullPath); // print absolute path
+		} else {
+			System.out.println(shell.getRootDir().name);
+			// print root directory name, since above prints nothing if currDir
+			// = root
+		}
+	}
 
 }
--- src/driver/PushDirOntoStack.java
+++ src/driver/PushDirOntoStack.java
@@ -2,6 +2,21 @@ package driver;
 
 public class PushDirOntoStack extends ShellCommand {
 
+	public static String getManual() {
+		return "pushd DIR \nSaves the current working directory by pushing "
+				+ "onto directory stack and then \n"
+				+ "changes the new current working directory to DIR. The push "
+				+ "must be \n"
+				+ "consistent as per the LIFO behavior of a stack. The pushd "
+				+ "command saves \n"
+				+ "the old current working directory in directory stack so that"
+				+ " it can be returned \n"
+				+ "to at any time (via the popd command). The size of the "
+				+ "directory stack \n"
+				+ "is dynamic and dependent on the pushd and the popd "
+				+ "commands.";
+	}
+	
 	public static void performOutcome(JShell shell, String[] parameters) {
 		// TODO Auto-generated method stub
 
