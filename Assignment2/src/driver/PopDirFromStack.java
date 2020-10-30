50e119b4028e7da2590c055be2fd7e2da15f77fe
wongnevi@UTORONTO.CA <wongnevi@UTORONTO.CA@dfdc42c8-2631-4d2a-ab77-34b069f44838>
2020-10-30 19:36:19 +0000

    Added manuals for popd, pushd, and pwd

--- Assignment2/src/driver/PopDirFromStack.java
+++ Assignment2/src/driver/PopDirFromStack.java
@@ -2,6 +2,18 @@ package driver;
 
 public class PopDirFromStack extends ShellCommand {
 
+	public static String getManual() {
+		return "popd \nRemove the top entry from the directory stack, and cd "
+				+ "into it. The removal \n"
+				+ "must be consistent as per the LIFO behavior of a stack. "
+				+ "The popd command \n"
+				+ "removes the top most directory from the "
+				+ "directory stack and makes it the \n"
+				+ "current working directory. If there is no directory onto "
+				+ "the stack, then give \n"
+				+ "appropriate error message.";
+	}
+	
 	public static void performOutcome(JShell shell, String[] parameters) {
 		// TODO Auto-generated method stub
 
