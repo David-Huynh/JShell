2135370dc07419c95e2cd3ac9d39dd73e6a0fbdc
huynhd12 <huynhd12@UTORONTO.CA>
2020-10-30 22:39:12 +0000

    Added base code and parser for Echo class aswell as created and filled out scrummeeting file.

--- src/driver/Echo.java
+++ src/driver/Echo.java
@@ -31,9 +31,63 @@
 package driver;
 
 public class Echo extends ShellCommand {
-
+	private static int contains(String[] parameters, String keyword){
+		int counter = 0;
+		for (int i = 1; i < parameters.length; i++){
+			if (parameters[i].contains(keyword)){
+				counter += parameters[i].length();
+			}
+		}
+		return counter;
+	}
+	private static String[] parseParameters(String [] parameters){
+		String parsedParams [] = {"",""};
+		boolean closedString = false;
+		for (int i = 1; i < parameters.length;i++) {
+			if (!closedString) {
+				if (parameters[i].charAt(0) == '>') {
+					closedString = true;
+					if(parameters[i].charAt(parameters[i].length()-1) != '>'){
+						parsedParams[1] = parameters[i].replace(">", "");
+					}
+				} else if (parameters[i].charAt(parameters[i].length() - 1) == '>') {
+					closedString = true;
+					parsedParams[0] += parameters[i].replace(">", "");
+					continue;
+				}
+				if (!closedString) {
+					parsedParams[0] += parameters[i].replace(">", "");
+				}
+			} else {
+				parsedParams[1] = parameters[i].replace(">", "");
+			}
+		}
+		return parsedParams;
+	}
+	public static String getManual() {
+		return "echo STRING [> OUTFILE]\n"
+				+ "If OUTFILE is not provided, print STRING on the shell.\n"
+				+ "Otherwise, put STRING into file OUTFILE.\n"
+				+ "STRING is a string of characters surrounded by double quotation marks.\n"
+				+ "This creates a new file if OUTFILE does not exists and erases "
+				+ "the old contents if OUTFILE already exists.\n "
+				+ "In either case, the only thing in OUTFILE should be STRING.\n"
+				+ "echo STRING >> OUTFILE\n"
+				+ "appends to OUTFILE instead of overwrite";
+	}
 	public static void performOutcome(JShell shell, String[] parameters) {
-		// TODO Auto-generated method stub
+		int numArrow = contains(parameters,">");
+		if (numArrow == 0){ //Print string to shell command
+			for (int i = 1; i < parameters.length; i++){
+				shell.println(parameters[i]);
+			}
+		}
+		String [] parsedParams = parseParameters(parameters);
+		if (numArrow == 1){//Overwrite file with string
+
+		}else{//Append to file with string
+
+		}
 
 	}
 
