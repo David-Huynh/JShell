3fae62d169eaf255fbf80771d325302f864ec4e2
chancol7@UTORONTO.CA <chancol7@UTORONTO.CA@dfdc42c8-2631-4d2a-ab77-34b069f44838>
2020-11-03 22:07:31 +0000

    adding checking for forbidden chars in MakeDirectory

--- Assignment2/src/driver/MakeDirectory.java
+++ Assignment2/src/driver/MakeDirectory.java
@@ -91,8 +91,14 @@ public class MakeDirectory extends ShellCommand {
 		for (int i = 0; i < dir.length; i++) {
 			if (i + 1 == dir.length) {
 				if (currDir.isSubDir(dir[i]) == -1) {
-					Directory newDir = new Directory(dir[i], currDir);
-					currDir.addFile(newDir);
+					if (!Directory.hasForbidChar(dir[i])) {
+						Directory newDir = new Directory(dir[i], currDir);
+						currDir.addFile(newDir);
+					} else {
+						PrintError.reportError(shell, "mkdir",
+								"Directory contains forbidden character(s): " + dir[i]);
+						return false;
+					}
 				} else {
 					PrintError.reportError(shell, "mkdir",
 							"Directory already exists: " + dir[i]);
