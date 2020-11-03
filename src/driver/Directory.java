77b1f2e2993f626359bd6edb3eb2d23ed83a5028
hejeff2 <hejeff2@UTORONTO.CA>
2020-11-03 23:43:58 +0000

    Fixed bug with cycleDir which was causing problems in ls

--- src/driver/Directory.java
+++ src/driver/Directory.java
@@ -34,118 +34,117 @@ import java.util.ArrayList;
 
 public class Directory extends StorageUnit {
 
-	private ArrayList<StorageUnit> contents = new ArrayList<StorageUnit>();
+  private ArrayList<StorageUnit> contents = new ArrayList<StorageUnit>();
 
-	public Directory(String name, Directory parentDir) {
-		this.name = name;
-		this.parentDir = parentDir;
-	}
-	
-	public ArrayList<StorageUnit> getDirContents() {
-		return contents;
-	}
+  public Directory(String name, Directory parentDir) {
+    this.name = name;
+    this.parentDir = parentDir;
+  }
 
-	public void addFile(StorageUnit fileName) {
-		contents.add(fileName);
-	}
+  public ArrayList<StorageUnit> getDirContents() {
+    return contents;
+  }
 
-	public void delFile(StorageUnit fileName) {
-		contents.remove(fileName);
-	}
+  public void addFile(StorageUnit fileName) {
+    contents.add(fileName);
+  }
 
-	StorageUnit getFile(int index) {
-		return contents.get(index);
-	}
+  public void delFile(StorageUnit fileName) {
+    contents.remove(fileName);
+  }
 
-	// function to check if a sub-directory named dirName is under Directory
-	// dir.
-	// returns -1 if sub-directory named dirName is not in dir or index of
-	// sub-dir if it is
-	public int isSubDir(String dirName) {
-		int index = -1;
-		for (int i = 0; i < contents.size(); i++) {
-			if (contents.get(i).getClass().getSimpleName()
-					.equals("Directory")) {
-				if (contents.get(i).name.equals(dirName)) {
-					index = i;
-				}
-			}
-		}
+  StorageUnit getFile(int index) {
+    return contents.get(index);
+  }
 
-		return index;
-	}
+  // function to check if a sub-directory named dirName is under Directory
+  // dir.
+  // returns -1 if sub-directory named dirName is not in dir or index of
+  // sub-dir if it is
+  public int isSubDir(String dirName) {
+    int index = -1;
+    for (int i = 0; i < contents.size(); i++) {
+      if (contents.get(i).getClass().getSimpleName().equals("Directory")) {
+        if (contents.get(i).name.equals(dirName)) {
+          index = i;
+        }
+      }
+    }
 
-	// function to check if a file named fileName is under Directory dir.
-	// returns -1 if file named dirName is not in dir or index of file if it is
-	public int containsFile(String fileName) {
-		int index = -1;
-		for (int i = 0; i < contents.size(); i++) {
-			if (contents.get(i).getClass().getSimpleName().equals("File")) {
-				if (contents.get(i).name.equals(fileName)) {
-					index = i;
-				}
-			}
-		}
+    return index;
+  }
 
-		return index;
-	}
+  // function to check if a file named fileName is under Directory dir.
+  // returns -1 if file named dirName is not in dir or index of file if it is
+  public int containsFile(String fileName) {
+    int index = -1;
+    for (int i = 0; i < contents.size(); i++) {
+      if (contents.get(i).getClass().getSimpleName().equals("File")) {
+        if (contents.get(i).name.equals(fileName)) {
+          index = i;
+        }
+      }
+    }
 
-	// function cycles through a path to see if the path is valid up to but not
-	// including the
-	// path.size() - finalIndex - 1 index of the path, returns null if path
-	// invalid, final
-	// directory/path checked by the function if valid
-	public static Directory cycleDir(ArrayList<String> path, int finalIndex,
-			Directory startDir) {
-		int index = 0;
-		int directoryIndex = -1;
+    return index;
+  }
 
-		if (path.get(index).equals("..") || path.get(index).equals(".")) {
-			if (path.get(index).equals("..")) {
-				directoryIndex = -2;
-			} else {
-				directoryIndex = -3;
-			}
-		} else {
-			directoryIndex = startDir.isSubDir(path.get(index));
-		}
+  // function cycles through a path to see if the path is valid up to but not
+  // including the
+  // path.size() - finalIndex - 1 index of the path, returns null if path
+  // invalid, final
+  // directory/path checked by the function if valid
+  public static Directory cycleDir(ArrayList<String> path, int finalIndex, Directory startDir, JShell shell) {
+    int index = 0;
+    int directoryIndex = -1;
 
-		while (directoryIndex != -1 && index < path.size() - finalIndex - 1) {
-			if (directoryIndex == -2) {
-				startDir = startDir.getParentDir();
-			} else if (directoryIndex == -3) {
+    if (path.get(index).equals("..") || path.get(index).equals(".")) {
+      if (path.get(index).equals("..")) {
+        directoryIndex = -2;
+      } else {
+        directoryIndex = -3;
+      }
+    } else {
+      directoryIndex = startDir.isSubDir(path.get(index));
+    }
 
-			} else {
-				startDir = (Directory) startDir.getDirContents()
-						.get(directoryIndex);
-			}
-			index++;
-			if (path.get(index).equals("..") || path.get(index).equals(".")) {
-				if (path.get(index).equals("..")) {
-					directoryIndex = -2;
-				} else {
-					directoryIndex = -3;
-				}
-			} else {
-				directoryIndex = startDir.isSubDir(path.get(index));
-			}
-		}
-		directoryIndex = startDir.getParentDir().isSubDir(startDir.getName());
-		if (directoryIndex != -1 || path.size() == 1) {
-			return startDir;
-		} else {
-			return null;
-		}
-	}
+    while (directoryIndex != -1 && index < path.size() - finalIndex - 1) {
+      if (directoryIndex == -2) {
+        startDir = startDir.getParentDir();
+      } else if (directoryIndex == -3) {
 
-	public static ArrayList<String> seperatePath(String path) {
-		String[] pathArray = path.split("/");
-		ArrayList<String> pathList = new ArrayList<String>();
+      } else {
+        startDir = (Directory) startDir.getDirContents().get(directoryIndex);
+      }
+      index++;
+      if (path.get(index).equals("..") || path.get(index).equals(".")) {
+        if (path.get(index).equals("..")) {
+          directoryIndex = -2;
+        } else {
+          directoryIndex = -3;
+        }
+      } else {
+        directoryIndex = startDir.isSubDir(path.get(index));
+      }
+    }
+    if(startDir != shell.getRootDir()) {
+      directoryIndex = startDir.getParentDir().isSubDir(startDir.getName());
+    }
+    if (directoryIndex != -1 || path.size() == 1) {
+      return startDir;
+    } else {
+      return null;
+    }
+  }
 
-		for (int i = 0; i < pathArray.length; i++) {
-			pathList.add(pathArray[i]);
-		}
+  public static ArrayList<String> seperatePath(String path) {
+    String[] pathArray = path.split("/");
+    ArrayList<String> pathList = new ArrayList<String>();
 
-		return pathList;
-	}
+    for (int i = 0; i < pathArray.length; i++) {
+      pathList.add(pathArray[i]);
+    }
+
+    return pathList;
+  }
 }
