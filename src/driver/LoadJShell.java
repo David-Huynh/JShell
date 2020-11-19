f4aaa1fa1b22ea90f174b98c513b802a3cd49a20
huynhd12 <huynhd12@UTORONTO.CA>
2020-11-19 02:16:40 +0000

    Added manual for LoadJShell.java and SaveJShell.java

--- src/driver/LoadJShell.java
+++ src/driver/LoadJShell.java
@@ -6,18 +6,25 @@ import java.io.ObjectInputStream;
 
 public class LoadJShell extends ShellCommand{
   public static String getManual() {
-    return "";
+    return "loadJShell localFilePath\n"
+        + "Loads a previously saved JShell session onto a fresh JShell sessions.\n"
+        + "This command only works on fresh JShell sessions.";
   }
   public static void performOutcome(JShell shell, String[] parameters) {
-    try{
-      ObjectInputStream in = new ObjectInputStream (new FileInputStream(parameters[1]));
-      JShell newShell = (JShell) in.readObject();
-      shell.updateShell(newShell);
-      in.close();
-    }catch (IOException e){
-      System.out.println("File not found");
-    }catch (ClassNotFoundException e){
-      System.err.println(e.getMessage());
+    if (shell.getComHis().size() == 1){
+      try{
+        ObjectInputStream in = new ObjectInputStream (new FileInputStream(parameters[1]));
+        JShell newShell = (JShell) in.readObject();
+        shell.updateShell(newShell);
+        in.close();
+      }catch (IOException e){
+        System.out.println("File not found");
+      }catch (ClassNotFoundException e){
+        System.err.println(e.getMessage());
+      }
+    }else {
+      PrintError.reportError(shell,"loadJShell: there are unsaved changes in this session,"
+          + "\nstart a fresh shell to load a previously saved session.");
     }
   }
 
