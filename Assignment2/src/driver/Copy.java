2144a365f951a11baed074d30c90a5a632748e23
hejeff2@UTORONTO.CA <hejeff2@UTORONTO.CA@dfdc42c8-2631-4d2a-ab77-34b069f44838>
2020-11-27 01:18:58 +0000

    Added Javadoc to Copy added honour code to TransferFile

--- Assignment2/src/driver/Copy.java
+++ Assignment2/src/driver/Copy.java
@@ -1,11 +1,57 @@
+// **********************************************************
+// Assignment2:
+
+// Student1: Collin Chan
+// UTORID user_name: chancol7
+// UT Student #: 1006200889
+// Author: Collin Chan
+//
+// Student2: Jeff He
+// UTORID user_name: Hejeff2
+// UT Student #: 1006398783
+// Author: Jeff He
+//
+// Student3: Nevin Wong
+// UTORID user_name: wongnevi
+// UT Student #: 1005391434
+// Author: Nevin Wong
+//
+// Student4: David Huynh
+// UTORID user_name: huynhd12
+// UT Student #: 1005991937
+// Author: David Huynh
+//
+//
+// Honor Code: I pledge that this program represents my own
+// program code and that I have coded on my own. I received
+// help from no one in designing and debugging my program.
+// I have also read the plagiarism section in the course info
+// sheet of CSC B07 and understand the consequences.
+// *********************************************************
+
 package driver;
 
+/**
+ * The Copy class is responsible for both copying and overwriting StorageUnits according to
+ * the user's specifications
+ */
 public class Copy extends ShellCommand {
 
+  /**
+   * Returns if this command produces StdOut. (used by the Interpreter to know whether or not to make
+   * a new file)
+   * 
+   * @return Whether or not the command produces StdOut
+   */
   public static boolean producesStdOut() {
     return false;
   }
-
+  
+  /**
+   * Provides the manual for how to use this command
+   *
+   * @return The manual
+   */
   public static String getManual() {
     return "mv OLDPATH NEWPATH\n"
         + "Copy item OLDPATH to NEWPATH. Both OLD-PATH and NEWPATH may " + "be "
@@ -13,6 +59,17 @@ public class Copy extends ShellCommand {
         + "NEWPATH is adirectory, copy \nthe item into the directory.";
   }
 
+  /**
+   * Tells the JShell to copy, overwrite or copy and overwrite, a StorageUnit to
+   * another StorageUnit
+   * 
+   * @param shell The JShell the command is to be performed on
+   * @param parameters The parameters from the interpreter the command is to work with
+   * @param outputType An integer representing the type of destination: 0 represents the command line,
+   *        1 represents overwriting a file, and 2 represents appending to a file
+   * @param outputFile If outputType is 1 or 2, this is the file we are overwriting/appending to,
+   *        otherwise null
+   */
   public static void performOutcome(JShell shell, String[] parameters,
       int outputType, File outputFile) {
 
@@ -42,6 +99,16 @@ public class Copy extends ShellCommand {
 
   }
 
+  /**
+   * Helper function for performOutcome, executes the case where the destination path (copyHere) is an
+   * existing StorageUnit, copies/overwrites toCopy to copyHere
+   * 
+   * @param toCopy The StorageUnit that is to be copied
+   * @param copyHere The StorageUnit where toCopy is copied to/overwritten
+   * @param copyHereParent The directory which contains copyHere
+   * @param shell The JShell where the command is being performed on
+   * @param parameters The parameters from the interpreter the command is to work with
+   */
   private static void executeExistingPath(StorageUnit copyHere,
       StorageUnit toCopy, JShell shell, String[] parameters) {
     if (copyHere.isDirectory()) {
@@ -57,14 +124,23 @@ public class Copy extends ShellCommand {
         return;
       }
     } else if (copyHere.isFile()) {
-      if (!TransferFile.validateFileToFile(copyHere, (File) copyHere, shell,
-          parameters)) {
+      if (!TransferFile.validateFileToFile(copyHere, shell, parameters)) {
         return;
       }
       copyAndOverwriteFile((File) toCopy, (File) copyHere);
     }
   }
 
+  /**
+   * Helper function for performOutcome, executes the case where the destination path (copyHere) is a
+   * new StorageUnit, copies and overwrites toCopy to copyCopy
+   * 
+   * @param toCopy The StorageUnit that is to be copied
+   * @param copyHere The StorageUnit where toCopy is to overwrite/create
+   * @param copyHereParent The directory which is to contain copyHere
+   * @param shell The JShell where the command is being performed on
+   * @param parameters The parameters from the interpreter the command is to work with
+   */
   private static void executeNewPath(StorageUnit copyHere, StorageUnit toCopy,
       Directory copyHereParent, JShell shell, String[] parameters) {
     StorageUnit copyHereNew =
@@ -83,11 +159,24 @@ public class Copy extends ShellCommand {
     }
   }
 
+  /**
+   * Copies StorageUnit toCopy inside of Directory copyHere
+   * 
+   * @param toCopy The StorageUnit to be copyd
+   * @param copyHere The destination of toCopy
+   */
   public static void copy(StorageUnit toCopy, Directory copyHere) {
     StorageUnit toAdd = toCopy.clone((Directory) copyHere);
     copyHere.getDirContents().add(toAdd);
   }
 
+  /**
+   * Copies StorageUnit toCopy inside Directory copyHereParent, renames toCopy to newName
+   * 
+   * @param toCopy The StorageUnit to be copied and renamed
+   * @param copyHereParent The directory toCopy is to be copied into
+   * @param newName The new name toCopy is given
+   */
   private static void copyAndOverwriteDir(StorageUnit toCopy,
       Directory copyHereParent, String newName) {
 
@@ -95,10 +184,24 @@ public class Copy extends ShellCommand {
     toCopy.setName(newName);
   }
 
+  /**
+   * Overwrites the contents of copyHere with the contents of toCopy
+   * 
+   * @param toCopy The file that is to overwrite copyHere
+   * @param copyHere The file is that is to be overwritten
+   */
+
   public static void copyAndOverwriteFile(File toCopy, File copyHere) {
     copyHere.overwrite(toCopy.getContents());
   }
 
+  /**
+   * Creates new File at copyHereParent with the contents of toCopy
+   * 
+   * @param toCopy The file that is to have its contents copied
+   * @param copyHereNew The file with the name of the file to be created
+   * @param copyHereParent The directory to hold the file to be created
+   */
   private static void copyAndCreateFile(File toCopy, File copyHereNew,
       Directory copyHereParent) {
 
