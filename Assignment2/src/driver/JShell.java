79cd1e20cf6bcd9f6114566b429db457b26ae281
wongnevi@UTORONTO.CA <wongnevi@UTORONTO.CA@dfdc42c8-2631-4d2a-ab77-34b069f44838>
2020-11-29 21:21:12 +0000

    New driver is the old 294 revision

---
+++ Assignment2/src/driver/JShell.java
@@ -0,0 +1,272 @@
+// **********************************************************
+// Assignment2:
+// Student1: Collin Chan
+// UTORID user_name: chancol7
+// UT Student #: 1006200889
+// Author: Collin Chan
+//
+// Student2: Jeff He
+// UTORID user_name: Hejeff2
+// UT Student #: 1006398783
+// Author: Jeff He
+//
+// Student3: Nevin Wong
+// UTORID user_name: wongnevi
+// UT Student #: 1005391434
+// Author: Nevin Wong
+//
+// Student4: David Huynh
+// UTORID user_name: huynhd12
+// UT Student #: 1005991937
+// Author: David Huynh
+//
+//
+// Honor Code: I pledge that this program represents my own
+// program code and that I have coded on my own. I received
+// help from no one in designing and debugging my program.
+// I have also read the plagiarism section in the course info
+// sheet of CSC B07 and understand the consequences.
+// *********************************************************
+
+package driver;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ * JShell is an interactive Unix-like shell that allows a user to use nineteen
+ * different commands to manipulate a virtual file system containing files and
+ * directories.
+ */
+
+public class JShell implements Serializable {
+
+	/** Root directory stores everything */
+	private Storage rootDir;
+	/** Current directory user is working in */
+	private Directory currentDir;
+	/** Whether the JShell is active, turned off if the user wishes to exit */
+	private transient boolean isActive;
+	/** Directory stack of the shell */
+	private Stack<Directory> dirStack;
+	/**
+	 * HashMap that maps commands to the Class that represents it e.g. ls maps
+	 * to ListFiles
+	 */
+	private HashMap<String, Class> cmdToClass;
+	/** ArrayList that stores the history of commands */
+	private ArrayList<String> comHis;
+
+	/**
+	 * Initializes an instance of the JShell, initializes all private variables
+	 */
+	public JShell() {
+		this.rootDir = Storage.createNewStorage();
+		this.currentDir = this.rootDir.getRoot(); // by default current
+													// directory is root
+		this.dirStack = new Stack<Directory>();
+		this.isActive = true;
+		this.cmdToClass = new HashMap<String, Class>();
+		this.populateCmdToClass();
+		this.comHis = new ArrayList<String>();
+	}
+
+	/**
+	 * Used by loadJShell command to update this JShell to a previously saved
+	 * one.
+	 * 
+	 * @param newShell
+	 *            The new JShell to up updated to.
+	 */
+	public void updateShell(JShell newShell) {
+		this.rootDir = newShell.rootDir;
+		this.currentDir = newShell.currentDir;
+		this.dirStack = newShell.dirStack;
+		this.cmdToClass = newShell.cmdToClass;
+		this.comHis = newShell.comHis;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		JShell jShell = (JShell) o;
+		return rootDir.equals(jShell.rootDir) &&
+				currentDir.equals(jShell.currentDir) &&
+				dirStack.equals(jShell.dirStack) &&
+				cmdToClass.equals(jShell.cmdToClass) &&
+				comHis.equals(jShell.comHis);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(rootDir, currentDir, dirStack, cmdToClass, comHis);
+	}
+
+	/**
+	 * Helper method for JShell initializer, populates the JShell's command to
+	 * Class HashMap
+	 */
+	private void populateCmdToClass() {
+		try { // Would need to update only this if/when there come new commands
+			this.cmdToClass.put("exit", Class.forName("driver.Exit"));
+			this.cmdToClass.put("mkdir", Class.forName("driver.MakeDirectory"));
+			this.cmdToClass.put("cd", Class.forName("driver.ChangeDirectory"));
+			this.cmdToClass.put("ls", Class.forName("driver.ListFiles"));
+			this.cmdToClass.put("pwd",
+					Class.forName("driver.PrintWorkingDirectory"));
+			this.cmdToClass.put("pushd",
+					Class.forName("driver.PushDirOntoStack"));
+			this.cmdToClass.put("popd",
+					Class.forName("driver.PopDirFromStack"));
+			this.cmdToClass.put("history",
+					Class.forName("driver.PrintHistory"));
+			this.cmdToClass.put("cat", Class.forName("driver.ConcatenateFile"));
+			this.cmdToClass.put("echo", Class.forName("driver.Echo"));
+			this.cmdToClass.put("man", Class.forName("driver.Manual"));
+			this.cmdToClass.put("saveJShell",
+					Class.forName("driver.SaveJShell"));
+			this.cmdToClass.put("loadJShell",
+					Class.forName("driver.LoadJShell"));
+			this.cmdToClass.put("search", Class.forName("driver.Search"));
+			this.cmdToClass.put("tree", Class.forName("driver.Tree"));
+			this.cmdToClass.put("rm", Class.forName("driver.Remove"));
+			this.cmdToClass.put("mv", Class.forName("driver.Move"));
+			this.cmdToClass.put("cp", Class.forName("driver.Copy"));
+			this.cmdToClass.put("curl", Class.forName("driver.ClientURL"));
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Public getter method for the current directory
+	 * 
+	 * @return The current directory of the shell
+	 */
+	public Directory getCurrentDir() {
+		return this.currentDir;
+	}
+
+	/**
+	 * Public setter method for the current directory
+	 * 
+	 * @param currentDir
+	 *            The given directory to be the new current directory
+	 */
+	public void setCurrentDir(Directory currentDir) {
+		this.currentDir = currentDir;
+	}
+
+	/**
+	 * Public getter method for the root directory
+	 * 
+	 * @return The root directory of the shell that lives in the single instance
+	 *         of Storage
+	 */
+	public Directory getRootDir() {
+		return this.rootDir.getRoot();
+	}
+
+	/**
+	 * Public getter method for the directory stack
+	 * 
+	 * @return The directory stack of the shell
+	 */
+	public Stack<Directory> getDirStack() {
+		return this.dirStack;
+	}
+
+	/**
+	 * Public getter method for the command to Class HashMap
+	 * 
+	 * @return The command to Class HashMap
+	 */
+	public HashMap<String, Class> getCmdToClass() {
+		return this.cmdToClass;
+	}
+
+	/**
+	 * Public getter method for the history of commands
+	 *
+	 * @return The ArrayList of the history of commands
+	 */
+	public ArrayList<String> getComHis() {
+		return this.comHis;
+	}
+
+	/**
+	 * Public setter method for appending to the history of commands
+	 * 
+	 * @param command
+	 *            The command to be added
+	 */
+	public void addCom(String command) {
+		this.comHis.add(command);
+	}
+
+	/**
+	 * Public exit method which the ShellCommand Exit uses
+	 */
+	public void exit() {
+		this.isActive = false;
+	}
+
+	/**
+	 * Prints a given message as a line on the shell
+	 * 
+	 * @param message
+	 *            The message to be printed as a line
+	 */
+	public void println(String message) {
+		System.out.println(message);
+	}
+
+	/**
+	 * Prints a given message as a on the shell
+	 * 
+	 * @param message
+	 *            The message to be printed
+	 */
+	public void print(String message) {
+		System.out.print(message);
+	}
+
+	/**
+	 * Prints a given error message on the shell
+	 * 
+	 * @param errMsg
+	 *            The error message to be printed
+	 */
+	public void printError(String errMsg) {
+		System.out.println(errMsg);
+	}
+
+	/**
+	 * Continually prompts the user for input and sends to Interpreter
+	 */
+	public void run() {
+		String userCommand;
+		Scanner userInput = new Scanner(System.in);
+		while (this.isActive) {
+			this.print(currentDir.getName() + ">");
+			userCommand = userInput.nextLine();
+			addCom(userCommand);
+			Interpreter.interpret(userCommand, this);
+		}
+		userInput.close();
+	}
+
+	/**
+	 * Creates a new instance of the JShell and runs it
+	 */
+	public static void main(String[] args) {
+		JShell shell = new JShell();
+		shell.run();
+	}
+}
---
+++ Assignment2/src/driver/MakeDirectory.java
@@ -0,0 +1,145 @@
+// **********************************************************
+// Assignment2:
+// Student1: Collin Chan
+// UTORID user_name: chancol7
+// UT Student #: 1006200889
+// Author: Collin Chan
+//
+// Student2: Jeff He
+// UTORID user_name: Hejeff2
+// UT Student #: 1006398783
+// Author: Jeff He
+//
+// Student3: Nevin Wong
+// UTORID user_name: wongnevi
+// UT Student #: 1005391434
+// Author: Nevin Wong
+//
+// Student4: David Huynh
+// UTORID user_name: huynhd12
+// UT Student #: 1005991937
+// Author: David Huynh
+//
+//
+// Honor Code: I pledge that this program represents my own
+// program code and that I have coded on my own. I received
+// help from no one in designing and debugging my program.
+// I have also read the plagiarism section in the course info
+// sheet of CSC B07 and understand the consequences.
+// *********************************************************
+
+package driver;
+
+/**
+ * The MakeDirectory command is used by the user to make new directories in the
+ * file system.
+ */
+
+public class MakeDirectory extends ShellCommand {
+
+	/**
+	 * Returns if this command produces StdOut. (used by the Interpreter to know
+	 * whether or not to make a new file)
+	 * 
+	 * @return Whether or not the command produces StdOut
+	 */
+	public static boolean producesStdOut() {
+		return false;
+	}
+	
+	/**
+	 * Provides the manual for how to use this command
+	 *
+	 * @return The manual
+	 */
+	public static String getManual() {
+		return "mkdir DIR ... \n" + "Create directories, "
+				+ "each of which may be relative to the current directory "
+				+ "or \nmay be a full path. If creating a DIR results in any "
+				+ "kind of error, do not proceed "
+				+ "\nwith creating the rest. However, "
+				+ "if some DIRs are created "
+				+ "successfully, and another \ncreation "
+				+ "results in an error, then give back an error "
+				+ "specific to this DIR.";
+	}
+
+	/**
+	 * Tell the JShell to make two new directories according to the user's
+	 * specifications.
+	 *
+	 * @param shell
+	 *            The JShell the command is to be performed on
+	 * @param parameters
+	 *            The parameters from the interpreter the command is to work
+	 *            with
+	 * @param outputType
+	 *            An integer representing the type of destination: 0 represents
+	 *            the command line, 1 represents overwriting a file, and 2
+	 *            represents appending to a file
+	 * @param outputFile
+	 *            If outputType is 1 or 2, this is the file we are
+	 *            overwriting/appending to, otherwise null
+	 */
+	public static void performOutcome(JShell shell, String[] parameters,
+			int outputType, File outputFile) {
+		if (parameters.length < 2) {
+			PrintError.reportError(shell, "mkdir",
+					"Invalid number of arguments.");
+			return;
+		}
+		Directory currDir;
+		Path path = new Path("");
+		for (int i = 1; i < parameters.length; i++) {
+			path.setPath(parameters[i]);
+			String[] elements = path.getPathElements();
+			if (path.isAbsolute()) {
+				currDir = shell.getRootDir();
+			} else {
+				currDir = shell.getCurrentDir();
+			}
+			Directory parent = path.cyclePath(0, currDir, shell);
+			//System.out.println("DIR NAME: "+parent.getName());
+			if (parent == null) {
+				PrintError.reportError(shell, "mkdir",
+						"Directory does not exits: " + parameters[i]);
+				return;
+			} else {
+				if (path.getPath().equals("/")) {
+					PrintError.reportError(shell, "mkdir",
+							"Root directory already exits!");
+					return;
+				} else {
+					if (parent.isSubDir(elements[elements.length - 1]) == -1) {
+						// create dir
+						if (!StorageUnit
+								.hasForbidChar(elements[elements.length - 1])) {
+							if (parent.containsFile(
+									elements[elements.length - 1]) == -1) {
+								Directory newDir = new Directory(
+										elements[elements.length - 1], parent);
+								parent.addFile(newDir);
+							} else {
+								PrintError.reportError(shell, "mkdir",
+										"Directory name cannot be the same as filename: "
+												+ elements[elements.length
+														- 1]);
+								return;
+							}
+						} else {
+							PrintError.reportError(shell, "mkdir",
+									"Directory name contains forbidden "
+											+ "character(s): "
+											+ elements[elements.length - 1]);
+							return;
+						}
+					} else {
+						PrintError.reportError(shell, "mkdir",
+								"Directory already exits: " + parameters[i]);
+						return;
+					}
+				}
+			}
+		}
+	}
+}
