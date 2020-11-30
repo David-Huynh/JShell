ce67c58541b75cbc429c70f45de395206e954a87
chancol7@UTORONTO.CA <chancol7@UTORONTO.CA@dfdc42c8-2631-4d2a-ab77-34b069f44838>
2020-11-30 05:02:20 +0000

    added test cases for MakeDirectoryTest

--- Assignment2/src/test/MakeDirectoryTest.java
+++ Assignment2/src/test/MakeDirectoryTest.java
@@ -2,6 +2,7 @@ package test;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -26,16 +27,14 @@ public class MakeDirectoryTest {
   }
 
   @Test
-  public void testPerformOutComeWithThreeRelative() {
-    String [] parameters = {"mkdir", "dir1", "dir2", "dir3"};
+  public void testPerformOutComeWithAbsAndRelative() {
+    String [] parameters = {"mkdir", "dir2", "/dir3", "dir4"};
     driver.MakeDirectory.performOutcome(shell, parameters, 0, null);
-    assertEquals("dir1", root.getDirContents().get(0).getName());
-    assertEquals(root, root.getDirContents().get(0).getParentDir());
     assertEquals("dir2", root.getDirContents().get(1).getName());
     assertEquals(root, root.getDirContents().get(1).getParentDir());
     assertEquals("dir3", root.getDirContents().get(2).getName());
     assertEquals(root, root.getDirContents().get(2).getParentDir());
+    assertEquals("dir4", root.getDirContents().get(3).getName());
+    assertEquals(root, root.getDirContents().get(3).getParentDir());
   }
-
-
 }
