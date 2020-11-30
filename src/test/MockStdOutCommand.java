efa030776198060fd3861cfecb51a53ee50b2ac0
wongnevi <wongnevi@UTORONTO.CA>
2020-11-30 21:17:48 +0000

    Added ShellCommandTest and satisfied Google Code for mkdir

--- src/test/MockStdOutCommand.java
+++ src/test/MockStdOutCommand.java
@@ -4,8 +4,12 @@ import driver.File;
 import driver.JShell;
 import driver.ShellCommand;
 
+/**
+ * Mock command that produces StdOut that is referred to as cmd1
+ */
 public class MockStdOutCommand extends ShellCommand {
 
+	public static JShell receivedShell;
 	public static String receivedParams[];
 	public static int receivedOutputType;
 	public static File receivedOutputFile;
@@ -21,6 +25,7 @@ public class MockStdOutCommand extends ShellCommand {
 	public static void performOutcome(JShell shell, String[] parameters,
 			int outputType, File outputFile) {
 		shell.println("I am cmd1 and I have just been called.");
+		receivedShell = shell;
 		receivedParams = parameters;
 		receivedOutputType = outputType;
 		receivedOutputFile = outputFile;
