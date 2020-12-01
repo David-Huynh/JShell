73c876c508c3638b2975e12dccabb334a90b4e89
wongnevi@UTORONTO.CA <wongnevi@UTORONTO.CA@dfdc42c8-2631-4d2a-ab77-34b069f44838>
2020-12-01 01:19:38 +0000

    Resurrected David's EchoTest file

---
+++ Assignment2/src/test/EchoTest.java
@@ -0,0 +1,104 @@
+package test;
+
+import driver.Echo;
+import driver.File;
+import driver.JShell;
+import driver.PushDirOntoStack;
+import driver.Storage;
+import driver.Tree;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.lang.reflect.Field;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class EchoTest {
+
+	JShell shell;
+	File stdOutFile;
+
+	/** Used to test print statements */
+	private final PrintStream printed = System.out;
+	private final ByteArrayOutputStream consoleStreamCaptor = 
+			new ByteArrayOutputStream();
+
+	/**
+	 * Set up a new JShell and a File to send StdOut to.
+	 */
+	@Before
+	public void setUp() {
+		shell = new JShell();
+		stdOutFile = new File("file", "", null);
+		System.setOut(new PrintStream(consoleStreamCaptor));
+	}
+
+	/**
+	 * Destroy the only reference to the storage system
+	 * 
+	 * @throws Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		System.setOut(printed);
+	}
+
+	/**
+	 * Test to see if the right manual is returned for getManual
+	 */
+	@Test
+	public void testGetManual() {
+		String manual = new String();
+		manual = Echo.getManual();
+		assertEquals(manual, "echo STRING [> OUTFILE]\n"
+				+ "If OUTFILE is not provided, print STRING on the shell.\n"
+				+ "Otherwise, put STRING into file OUTFILE.\n"
+				+ "STRING is a string of characters "
+				+ "surrounded by double quotation marks.\n"
+				+ "This creates a new file if "
+				+ "OUTFILE does not exists and erases "
+				+ "the old contents if OUTFILE already exists.\n"
+				+ "In either case, the only "
+				+ "thing in OUTFILE should be STRING.\n"
+				+ "echo STRING >> OUTFILE\n" + "Like the previous command, but"
+				+ "appends to OUTFILE instead of overwrites");
+	}
+
+	@Test
+	public void testPerformOutcomeOneParam() {
+		String[] parameters = {"echo"};
+		Echo.performOutcome(shell, parameters, 1, stdOutFile);
+		assertEquals("echo: invalid number of parameters",
+				consoleStreamCaptor.toString().trim());
+	}
+
+	@Test
+	public void testPerformOutcomeThreeParams() {
+		String[] parameters = {"echo", "this", "that"};
+		Echo.performOutcome(shell, parameters, 1, stdOutFile);
+		assertEquals("echo: invalid number of parameters",
+				consoleStreamCaptor.toString().trim());
+	}
+
+	@Test
+	public void testPerformOutcomeEchoingAWord() {
+		String[] parameters = {"echo", "\"PHLEBOTINUM\""};
+		Echo.performOutcome(shell, parameters, 1, stdOutFile);
+		assertEquals("PHLEBOTINUM", stdOutFile.getContents());
+	}
+
+	@Test
+	public void testPerformOutcomeEchoingAPhraseWithWhitespace() {
+		String[] parameters = {"echo",
+				"\"    My name is Amazon Echo and here is some "
+						+ "space            \""};
+		Echo.performOutcome(shell, parameters, 1, stdOutFile);
+		assertEquals("    My name is Amazon Echo and here is some space    "
+				+ "        ", stdOutFile.getContents());
+	}
+}
