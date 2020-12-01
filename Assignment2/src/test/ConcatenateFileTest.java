1eac043feaa77a18b33d5a7ae402e46a071e4e4d
wongnevi@UTORONTO.CA <wongnevi@UTORONTO.CA@dfdc42c8-2631-4d2a-ab77-34b069f44838>
2020-12-01 04:11:05 +0000

    Satisfied Google Code for CatTest

--- Assignment2/src/test/ConcatenateFileTest.java
+++ Assignment2/src/test/ConcatenateFileTest.java
@@ -39,62 +39,63 @@ import org.junit.Test;
 import driver.*;
 
 public class ConcatenateFileTest {
-  JShell shell;
-  Directory root;
-  Directory dir1;
-  File file1;
-  File stdOutFile;
+	
+	JShell shell;
+	Directory root;
+	Directory dir1;
+	File file1;
+	File stdOutFile;
 
-  /** Used to test print statements */
-  private final PrintStream printed = System.out;
-  private final ByteArrayOutputStream consoleStreamCaptor =
-      new ByteArrayOutputStream();
+	/** Used to test print statements */
+	private final PrintStream printed = System.out;
+	private final ByteArrayOutputStream consoleStreamCaptor = 
+			new ByteArrayOutputStream();
 
-  @Before
-  public void setUp() {
-    shell = new JShell();
-    root = shell.getRootDir();
-    dir1 = new Directory("dir1", root);
-    file1 = new File("file1", "123", dir1);
-    stdOutFile = new File("file", "", null);
-    System.setOut(new PrintStream(consoleStreamCaptor));
-  }
+	@Before
+	public void setUp() {
+		shell = new JShell();
+		root = shell.getRootDir();
+		dir1 = new Directory("dir1", root);
+		file1 = new File("file1", "123", dir1);
+		stdOutFile = new File("file", "", null);
+		System.setOut(new PrintStream(consoleStreamCaptor));
+	}
 
-  @Test
-  public void testGetManual() {
-    assertEquals("cat FILE1 [FILE2 ...] \n"
-        + "Display the contents of FILE1 and other files "
-        + "(i.e. File2 ...) concatenated in \n"
-        + "the shell. You may want to "
-        + "use three line breaks to separate "
-        + "the contents of one file \n" + "from the other file.",
-        driver.ConcatenateFile.getManual());
-  }
+	@Test
+	public void testGetManual() {
+		assertEquals("cat FILE1 [FILE2 ...] \n"
+				+ "Display the contents of FILE1 and other files "
+				+ "(i.e. File2 ...) concatenated in \n"
+				+ "the shell. You may want to "
+				+ "use three line breaks to separate "
+				+ "the contents of one file \n" + "from the other file.",
+				driver.ConcatenateFile.getManual());
+	}
 
-  @Test
-  public void testPerformOutComeWithValidRelPath() {
-    root.addFile(dir1);
-    dir1.addFile(file1);
-    String [] parameters = {"cat", "dir1/file1"};
-    driver.ConcatenateFile.performOutcome(shell, parameters, 1, stdOutFile);
-    assertEquals("123", stdOutFile.getContents());
-  }
+	@Test
+	public void testPerformOutComeWithValidRelPath() {
+		root.addFile(dir1);
+		dir1.addFile(file1);
+		String[] parameters = {"cat", "dir1/file1"};
+		driver.ConcatenateFile.performOutcome(shell, parameters, 1, stdOutFile);
+		assertEquals("123", stdOutFile.getContents());
+	}
 
-  @Test
-  public void testPerformOutComeWithValidAbsPath() {
-    root.addFile(dir1);
-    dir1.addFile(file1);
-    String [] parameters = {"cat", "/dir1/file1"};
-    driver.ConcatenateFile.performOutcome(shell, parameters, 1, stdOutFile);
-    assertEquals("123", stdOutFile.getContents());
-  }
+	@Test
+	public void testPerformOutComeWithValidAbsPath() {
+		root.addFile(dir1);
+		dir1.addFile(file1);
+		String[] parameters = {"cat", "/dir1/file1"};
+		driver.ConcatenateFile.performOutcome(shell, parameters, 1, stdOutFile);
+		assertEquals("123", stdOutFile.getContents());
+	}
 
-  @Test
-  public void testPerformOutComeWithMultiplePaths() {
-    root.addFile(dir1);
-    dir1.addFile(file1);
-    String [] parameters = {"cat", "/dir1/file1", "../dir1/file1"};
-    driver.ConcatenateFile.performOutcome(shell, parameters, 1, stdOutFile);
-    assertEquals("123\n\n\n\n123", stdOutFile.getContents());
-  }
+	@Test
+	public void testPerformOutComeWithMultiplePaths() {
+		root.addFile(dir1);
+		dir1.addFile(file1);
+		String[] parameters = {"cat", "/dir1/file1", "../dir1/file1"};
+		driver.ConcatenateFile.performOutcome(shell, parameters, 1, stdOutFile);
+		assertEquals("123\n\n\n\n123", stdOutFile.getContents());
+	}
 }
